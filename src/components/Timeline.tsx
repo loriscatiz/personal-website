d58e6e3aaae15608928d40aa755a58efb9ce6375
loriscatiz <loriscatiz@gmail.com>
2025-01-13 18:48:31 +0100

    timeline content and layout

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -2,6 +2,86 @@ import TimelineCard from './TimelineCard';
 import Title from './Title';
 
 function Timeline() {
+    const timelineEvents: {
+        position: string;
+        company: string;
+        time: string;
+        tag: 'work' | 'education'; // Ensure this matches
+        description: string;
+        imgSrc: string;
+        index: number;
+    }[] = [
+        {
+            position: 'IFTS Software developer',
+            company: 'Ifoa',
+            time: 'November 2024 - Present',
+            tag: 'education',
+            description:
+                "I'm currently deepening my understanding of software development, focusing on databases (MySQL), backend development (Java and Python), and frontend technologies (HTML, CSS, and JavaScript). While I already had experience with frontend development, this course emphasizes not just learning syntax but also structuring and designing software thoughtfully. It's teaching me to think like a developer and focus on creating solutions that are both effective and maintainable.",
+            imgSrc: '/IFOA_Logo.png',
+            index: 1,
+        },
+        {
+            position: 'Helpdesk',
+            company: 'GPI Group - (via Randstad)',
+            time: 'July 2024 - August 2024',
+            tag: 'work',
+            description:
+                "In this role, I provided technical support to healthcare professionals, helping them navigate and resolve issues with their CRM software. While not directly related to coding, this experience allowed me to develop strong teamwork and interpersonal communication skills. Though brief, it remains a valuable part of my professional journey, showcasing my adaptability and ability to collaborate in high-pressure environments.",
+            imgSrc: '/logo-GPI-2019.jpg',
+            index: 2,
+        },
+        {
+            position: 'WordPress Web Developer Internship',
+            company: 'Reddoak - meravigliä',
+            time: 'November 2023 - May 2024',
+            tag: 'work',
+            description:
+                "During this internship, I built websites using Elementor, created designs in Figma, and occasionally wrote custom code (CSS and JavaScript) to overcome CMS limitations. This experience taught me how to approach problem-solving in real-world scenarios, work efficiently under tight deadlines, and collaborate effectively in a team. It was my first real work experience and a key step in solidifying my passion for web development.",
+            imgSrc: '/logo_reddoak_foglia.png',
+            index: 3,
+        },
+        {
+            position: 'WordPress Web Developer Internship',
+            company: 'meravigliä - Reddoak',
+            time: 'April 2023 - September 2023',
+            tag: 'education',
+            description:
+                "This course was my introduction to the world of web development, sparking a passion for creating functional and visually appealing websites. Although the primary focus was graphic design, it laid the foundation for my future career in tech and connected me to my first internship as a WordPress developer.",
+            imgSrc: '/IFOA_Logo.png',
+            index: 4,
+        },
+      
+      
+    ];
+
+    const maptimelineEvents = (
+        timelineEvents: {
+            position: string;
+            company: string;
+            time: string;
+            tag: 'work' | 'education';
+            description: string;
+            imgSrc: string;
+            index: number;
+        }[]
+    ) => {
+        return timelineEvents.map((e) => {
+            return (
+                <TimelineCard
+                    position={e.position}
+                    company={e.company}
+                    time={e.time}
+                    tag={e.tag}
+                    description={e.description}
+                    imgSrc={e.imgSrc}
+                    index={e.index}
+                    key={e.index}
+                />
+            );
+        });
+    };
+
     return (
         <div className="bg-sky-200 py-16 dark:bg-sky-900" id="timeline">
             <div className="container">
@@ -17,17 +97,9 @@ function Timeline() {
                     define my growth as a developer. Each card tells a story of
                     learning, building, and evolving along the way.
                 </p>
-                <div className="timeline-cards-wrapper"></div>
-                <TimelineCard
-                    position={'IFTS'}
-                    company={'Ifoa'}
-                    time={'Nov 2024 - June 2025'}
-                    tag={'work'}
-                    description={
-                        'Placeholder Explore the journey that has shaped my skills and Placeholder Explore the journey that has shaped my skills and Placeholder Explore the journey that has shaped my skills and'
-                    }
-                    imgSrc={''}
-                ></TimelineCard>
+                <div className="timeline-cards-wrapper relative mt-8 grid gap-6 lg:grid-cols-2">
+                    {maptimelineEvents(timelineEvents)}
+                </div>
             </div>
         </div>
     );
--- src/components/TimelineCard.tsx
+++ src/components/TimelineCard.tsx
@@ -4,9 +4,10 @@ interface timelineProps {
     position: string;
     company: string;
     time: string;
-    tag: 'work' | 'study';
+    tag: 'work' | 'education';
     description: string;
     imgSrc: string;
+    index: number;
 }
 
 function TimelineCard({
@@ -16,11 +17,16 @@ function TimelineCard({
     time,
     description,
     tag,
+    index,
 }: timelineProps) {
     return (
-        <div className="timeline-card border-4 border-solid border-indigo-600">
-            <div className="timeline-card-header grid items-center bg-sky-900 px-8 py-6 text-sky-100 dark:bg-sky-200 dark:text-sky-950">
-                <img src={imgSrc} alt={company} />
+        <div
+            className={`timeline-card z-10 text-sky-900 dark:text-sky-200 lg:row-span-3 ${index == 1 ? 'lg:row-start-1' : index == 2 ? 'lg:row-start-3' : index == 3 ? 'lg:row-start-5' : index == 4 ? 'lg:row-start-7' : ''} /* only on big screens, make a ladder layout left right*/`}
+        >
+            <div
+                className={`timeline-card-header relative grid items-center gap-4 rounded-t-3xl border-4 border-b-0 border-solid border-indigo-600 bg-sky-900 px-6 py-4 text-sky-100 dark:bg-sky-200 dark:text-sky-950 md:px-8 md:py-6 ${index % 2 == 0 ? 'lg:after:left-0 lg:after:-translate-x-full' : 'lg:after:right-0 lg:after:translate-x-full'}`}
+            >
+                <img src={imgSrc} alt={company} className="w-full bg-white aspect-square p-1 object-contain rounded-full" />
                 <div>
                     <Title
                         tag={'h3'}
@@ -33,10 +39,18 @@ function TimelineCard({
                         className="text-lg"
                     ></Title>
                 </div>
-                <span className="text-right">{time}</span>
+                <div
+                    className={`col-span-2 text-right pt-2 px-2 border-t-2 border-sky-200 dark:border-sky-900 ${index % 2 == 0 && 'lg:text-left'}`}
+                >
+                   <span > {time} </span>
+                </div>
             </div>
-            <div className="timeline-card-body">{description}</div>
-            <div className="timeline-card-footer">{tag}</div>
+            <div className="timeline-card-body border-4 border-y-0 border-solid border-indigo-600 bg-sky-100 px-8 py-6 dark:bg-sky-950">
+                <p>{description}</p>
+            </div>
+            <div className={`timeline-card-footer rounded-b-3xl border-4 border-t-0 border-solid border-indigo-600 bg-sky-100 px-8 pb-6 text-sm dark:bg-sky-950 text-right ${index % 2 == 0 && 'lg:text-left'}`}>
+                <span className='border-b-2 border-indigo-600'>{tag}</span>
+            </div>
         </div>
     );
 }
