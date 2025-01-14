eeda1f6ee640344839b74d73915df9a8542182a6
loriscatiz <loriscatiz@gmail.com>
2025-01-14 12:35:17 +0100

    basic contact content

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,11 +3,17 @@ import Title from './Title';
 function Contact() {
     return (
         <div className="bg-sky-100 py-16 dark:bg-sky-950" id="contact">
-            <Title
-                tag={'h2'}
-                className="text-center text-2xl text-sky-950 dark:text-sky-100 md:text-4xl"
-                text="Contact me"
-            ></Title>
+            <div className="container grid grid-cols-2">
+                <Title
+                    tag={'h2'}
+                    className="col-span-2 gap-x-8 text-center text-2xl text-sky-950 dark:text-sky-100 md:text-4xl"
+                    text="Contact me"
+                ></Title>
+                <p className="mt-6 text-xl text-sky-900 dark:text-sky-200">
+                    Got a question or want to collaborate? Feel free to reach
+                    out!
+                </p>
+            </div>
         </div>
     );
 }
